1f9378b7111b570fa5b4dddb2a0acb7773c3e561
KwietniewskiK <55291405+KwietniewskiK@users.noreply.github.com>
2019-09-13 19:58:34 +0200

    Add files via upload

---
+++ Index.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en" dir="ltr">
+  <head>
+    <meta charset="utf-8">
+    <title></title>
+  </head>
+  <body>
+    <<?php
+    $name =  'janusz';
+    $name =  'janusz';
+    $name =  'janusz';
+
+    echo "ZSł";
+
+
+echo ("string<br>");
+echo ("string<br>");
+echo "string<br>";
+echo "string<br>";
+echo 18;
+echo "<br>Masz",18, "lat"
+print "zsł";
+echo "<h1 style=\"color:blue\">tekst</h1>";
+     ?>
+  </body>
+</html>
